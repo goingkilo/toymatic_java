29adb202f2d8581c520c95be6c5ee9d65f421427
Karthik Raghunathan <kraghunathan@groupon.com>
2018-12-11 15:40:23 +0530

    fixed clob-not-implemented startup error

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,13 +1,9 @@
 package com.example;
 
-import com.example.jpa.Task;
-import com.example.jpa.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -17,10 +13,7 @@ public class Application {
     public static void main(String[] args) {
 
         log.info("Welcome to the new millenium !!");
-
         SpringApplication.run(Application.class, args);
     }
-
-
 
 }
--- src/main/java/com/example/security/WebSecurityConfig.java
+++ src/main/java/com/example/security/WebSecurityConfig.java
@@ -1,14 +1,14 @@
 package com.example.security;
 
 import org.springframework.context.annotation.Bean;
-        import org.springframework.context.annotation.Configuration;
-        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-        import org.springframework.security.core.userdetails.User;
-        import org.springframework.security.core.userdetails.UserDetails;
-        import org.springframework.security.core.userdetails.UserDetailsService;
-        import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
