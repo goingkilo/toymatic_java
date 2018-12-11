4aac9364771dcaf0dc5cad3f40809e2ac2275fe2
Karthik Raghunathan <kraghunathan@groupon.com>
2018-12-11 21:57:13 +0530

    added transactional to one task query

--- src/main/java/com/example/jpa/TaskRepository.java
+++ src/main/java/com/example/jpa/TaskRepository.java
@@ -1,13 +1,16 @@
 package com.example.jpa;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
 
+    @Transactional
     List<Task> findByDescription(String description);
+    
     List<Task> findByOwner(String owner);
 
 }
