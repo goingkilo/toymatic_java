d1da93aa9e9bfe0af62272fcd6133910fac12fa3
Karthik Raghunathan <kraghunathan@groupon.com>
2018-12-15 18:39:22 +0530

    checkpoint 1

--- src/main/java/com/example/jms/JmsConfig.java
+++ src/main/java/com/example/jms/JmsConfig.java
@@ -1,25 +1,25 @@
-package com.example.jms;
-
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-
-import javax.jms.ConnectionFactory;
-
-@Configuration
-public class JmsConfig {
-
-    @Bean
-    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
-                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        configurer.configure(factory, connectionFactory);
-        return factory;
-    }
-
-    @Bean Sender createSender(){
-        return new Sender();
-    }
-}
+//package com.example.jms;
+//
+//import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+//import org.springframework.jms.config.JmsListenerContainerFactory;
+//
+//import javax.jms.ConnectionFactory;
+//
+//@Configuration
+//public class JmsConfig {
+//
+//    @Bean
+//    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+//                                                    DefaultJmsListenerContainerFactoryConfigurer configurer) {
+//        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+//        configurer.configure(factory, connectionFactory);
+//        return factory;
+//    }
+//
+//    @Bean Sender createSender(){
+//        return new Sender();
+//    }
+//}
--- src/main/java/com/example/jms/Receiver.java
+++ src/main/java/com/example/jms/Receiver.java
@@ -1,14 +1,14 @@
-package com.example.jms;
-
-import org.springframework.jms.annotation.JmsListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public class Receiver {
-
-    @JmsListener(destination = "topic1", containerFactory = "myFactory")
-    public void receiveMessage(String message) {
-        System.out.println("Received <" + message + ">");
-    }
-
-}
+//package com.example.jms;
+//
+//import org.springframework.jms.annotation.JmsListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class Receiver {
+//
+//    @JmsListener(destination = "topic1", containerFactory = "myFactory")
+//    public void receiveMessage(String message) {
+//        System.out.println("Received <" + message + ">");
+//    }
+//
+//}
