ce92dc16eda7d4fd370fbb3eae83d5b88e607a59
Karthik Raghunathan <kraghunathan@groupon.com>
2018-12-15 18:45:05 +0530

    pared down web, rest, jpa version

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableScheduling
-//@EnableJms
 public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
